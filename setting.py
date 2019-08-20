d985de3d439d18a5aef5cafd909b8cd380748e5e
Chen <286298650@qq.com>
2019-08-20 18:33:01 +0800

    查询功能测试。

--- setting.py
+++ setting.py
@@ -1,13 +1,16 @@
 from sqlalchemy import create_engine #mysql连接函数
+from sqlalchemy.orm import sessionmaker
 
 #mysql连接配置
 connect_sql=create_engine(
     #mysql连接配置
     "mysql+pymysql://root:bai940126@111.67.196.17/Mysite",
     encoding="utf-8",  #编码
-    # echo=True    #打印结果
+    #echo=True    #打印结果
     )
 
+session = sessionmaker(bind=connect_sql)
+Session = session()  # 实例化会话对象
 
 """
     1、该文件为mysql连接的公共配置文件，供所有需要连接的文件调用。
--- views.py
+++ views.py
@@ -1,8 +1,15 @@
 from Insert import Insert_into
+from setting import Session
 from create_table import student
 
-#写入数据，没有写入的则使用默认值
-DATA=student(name="chen",passwd='hao1',sex='1',age=18,num=12)
-
-#调用封装的函数，写入mysql表中
-Insert_into(DATA)
+# #写入数据，没有写入的则使用默认值
+# DATA=student(name="chen",passwd='hao1',sex='1',age=18,num=12)
+# #调用封装的函数，写入mysql表中
+# Insert_into(DATA)
+#
+# AA=Session.query(student.age).filter(student.id>5).all()
+AA=Session.query(student.id).all()
+if AA:
+  print(AA)
+else:
+  print("on")
